69a560fc1dc229c1a54aace262d02100c9ca5fc3
Laikokwei <kokwei325@gmail.com>
2022-09-14 16:58:33 +0800

    added parallax style home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,50 +92,89 @@ const Home: NextPage = () => {
           </div>
         </div>
       </nav>
+      <main>
+        <header
+          className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img"
+        >
+          <div className="p-5 text-3xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </div>
+        </header>
+        <div className="max-w-lg m-auto">
+          <p className="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat a
+            magna non varius. Proin leo felis, euismod non porta eget, varius sit amet
+            sapien. Maecenas in nulla at leo convallis consectetur id a sapien. Nulla
+            nec pulvinar nisi. Vivamus non facilisis lacus, et volutpat libero. Nulla ac
+            odio aliquam, accumsan arcu ut, lacinia est. Nulla eu sem elit. Fusce nec
+            laoreet sem, semper molestie libero.
+          </p>
+          <p className="mb-4">
+            Ut sagittis lacus consequat accumsan venenatis. Sed sollicitudin, lectus et
+            fringilla ultrices, dolor nisi scelerisque tortor, vel finibus magna massa
+            non nunc. Phasellus massa quam, egestas a nisl sed, porta volutpat metus.
+            Nunc sed elit ac tellus tempor cursus. Suspendisse potenti. Vestibulum
+            varius rutrum nisl nec consequat. Suspendisse semper dignissim sem viverra
+            semper. Nulla porttitor, purus nec accumsan pharetra, nisi dolor condimentum
+            ipsum, id consequat nulla nunc in ligula.
+          </p>
+          <p className="mb-12">
+            Nulla pharetra lacinia nisi, vitae mollis tellus euismod id. Mauris porta
+            dignissim hendrerit. Cras id velit varius, fermentum lectus vitae, ultricies
+            dolor. In bibendum rhoncus purus vel rutrum. Nam vulputate imperdiet
+            fringilla. Donec blandit libero massa. Suspendisse dictum diam mauris, vitae
+            fermentum dolor tincidunt in. Pellentesque sollicitudin venenatis dolor,
+            vitae scelerisque elit ultrices eu. Donec eget sodales risus, quis dignissim
+            neque.
+          </p>
+        </div>
+        <section
+          className="container flex items-center justify-center h-screen m-auto mb-12 bg-fixed bg-center bg-cover custom-img"
+        >
+          <div className="p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
+            Parralax inline
+          </div>
+        </section>
 
-      <main className={styles.main}>
+        <div className="flex flex-col items-center justify-center flex-grow min-h-screen px-0 py-16">
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
 
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <p className={styles.description}>
+            Get started by editing{' '}
+            <code className={styles.code}>pages/index.tsx</code>
+          </p>
 
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+          <div className={styles.grid}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h2>Documentation &rarr;</h2>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h2>Learn &rarr;</h2>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <a
+              href="https://github.com/vercel/next.js/tree/canary/examples"
+              className={styles.card}
+            >
+              <h2>Examples &rarr;</h2>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h2>Deploy &rarr;</h2>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
         </div>
       </main>
 
